4980aa91b5b5ca18000b1f4ffd361dd53d84adf1
Nousheen Begum <111900257+Nousheenb@users.noreply.github.com>
2023-01-03 04:55:47 +0530

    Update 1.create_traverse.cpp

--- Linked list/1.create_traverse.cpp
+++ Linked list/1.create_traverse.cpp
@@ -1,72 +1,47 @@
-//creating a linked list and traversing through it 
-
-     
-#include<iostream>
+#include <iostream>
 using namespace std;
-
-//creating a class to create node type of objects
-
-class Node
-{
-	public:
-		int data;
-		Node *next;
+ 
+class Node{
+public:
+    int data;
+    Node* next;
 };
-
-class LinkedList
-{
-	private:
-		Node *first;
-	public:
-		LinkedList(){
-			first = NULL;
-		}
-		
-		LinkedList(int arr[] , int n);   //constructor
-		void display();
-		
-};
-
-
-//creating a linked list
-LinkedList :: LinkedList(int arr[] , int n)
-{
-	
-	first = new Node ;
-	first->data = arr[0];
-	first->next = NULL;
-	Node *last ;
-	last = first;
-	
-	for (int i=1 ; i<n ;i++)
-	{
-		Node *temp;
-		temp = new Node ;
-		temp->data = arr[i] ;
-		temp->next = NULL;
-		last->next = temp ;
-		last = temp;
-	}
-}
-
-void LinkedList :: display() {
-	
-	Node *p;
-	p = first ;
-	while ( p ) { 
-		cout << p->data << " ";
-		p = p->next ;    //updating of pointer
-	}
-}
-
-
-int main()
-{
-	int arr[10] = {99,89,79,48,5} ;
-	
-	LinkedList l(arr , 5);
-	
-	l.display();
-	 
-	return 0;
+ 
+int main() {
+ 
+    int A[] = {3, 5, 7, 10, 15};
+ 
+    Node* head = new Node;
+ 
+    Node* temp;
+    Node* last;
+ 
+    head->data = A[0];
+    head->next = nullptr;
+    last = head;
+ 
+    // Create a Linked List
+    for (int i=1; i<5; i++){
+ 
+        // Create a temporary Node
+        temp = new Node;
+ 
+        // Populate temporary Node
+        temp->data = A[i];
+        temp->next = nullptr;
+ 
+        // last's next is pointing to temp
+        last->next = temp;
+        last = temp;
+    }
+ 
+    // Display Linked List
+    Node* p = head;
+ 
+    while (p != nullptr){
+        cout << p->data << " -> "<<flush;
+        p = p->next;
+    }
+ 
+    return 0;
 }
